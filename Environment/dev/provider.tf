af424534a895c69460f64359e6bf97441fd6715a
Pardeep <yadavpradeep5592@gmail.com>
2025-12-09 06:14:07 +0530

    pipeline

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -5,15 +5,15 @@ terraform {
       version = "4.52.0"
     }
   }
-  backend "azurerm" {
-    storage_account_name = "monotodostg12345"
-    resource_group_name  = "dev-rg"
-    container_name       = "todo-cnt"
-    key                  = "dev.todomicro.tfstate"
-  }
+  # backend "azurerm" {
+  #   storage_account_name = "monotodostg123456"
+  #   resource_group_name  = "dev-rg"
+  #   container_name       = "todo-cnt"
+  #   key                  = "dev.todomicro.tfstate"
+  # }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "82a45200-ef80-4da2-a03b-18d9a77b29b7"
+  subscription_id = "a292df9f-279e-490f-bac2-c8a203d362bc"
 }
